180d41286ef3547e297ac9214007811df2d36309
Armanur Rahman <mubeenarman19@gmail.com>
2021-10-24 16:34:25 +0900

    Update tailwind.config.js
    add configuration in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,34 @@ module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            tablet: "640px",
+            desktop: "1000px",
+        },
+        extend: {
+            colors: {
+                primaryLight: "#a5d8ff",
+                primaryMain: "#339af0",
+                primaryDark: "#1864ab",
+                secondaryLight: "#ffe8cc",
+                secondaryMain: "#ff922b",
+                secondaryDark: "#d9480f",
+            },
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        function ({ addComponents }) {
+            addComponents({
+                ".container": {
+                    maxWidth: "100%",
+                    "@screen desktop": {
+                        maxWidth: "1300px",
+                    },
+                },
+            });
+        },
+    ],
 };
